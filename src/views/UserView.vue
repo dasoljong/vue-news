cdf17fa4e3b99cd1ce4f92d5e1351841be4d164f
DasolJong <dasoljong92@gmail.com>
2021-05-23 00:11:20 +0900

    userProfile

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
-    <p v-if="userInfo.about">
-      about : {{ userInfo.about }}
-    </p>
-    <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <user-profile></user-profile>
+<!--    <p>name : {{ userInfo.id }}</p>-->
+<!--    <p v-if="userInfo.about">-->
+<!--      about : {{ userInfo.about }}-->
+<!--    </p>-->
+<!--    <p>karma : {{ userInfo.karma }}</p>-->
+<!--    <p>created : {{ userInfo.created }}</p>-->
   </div>
 </template>
 
 <script>
+import UserProfile from "@/components/UserProfile.vue";
+
 export default {
+  components: {
+    UserProfile,
+  },
   computed:{
     userInfo(){
       return this.$store.state.user;
